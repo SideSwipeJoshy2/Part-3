17f680070ae1b9d177832269bb29865c844772d0
DESKTOP-APTFB7I\Joshua <hargobij@sheridancollege.ca>
2024-03-21 15:08:25 -0400

    task 4 complete

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -3,11 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.TerrainTools;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterControl : MonoBehaviour
 {
     public TMPro.TextMeshProUGUI currentSelection;
     public static CharacterControl Instance;
+    float interpolation;
+    public List<Villager> villagerpicklist;
     public static Villager SelectedVillager { get; private set; }
     public static void SetSelectedVillager(Villager villager)
     {
@@ -18,6 +21,11 @@ public class CharacterControl : MonoBehaviour
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
         Instance.currentSelection.text = villager.ToString();
+    }
+
+    public void DropdownSelectionHasChanged(int pick)
+    {
+        SetSelectedVillager(villagerpicklist[pick]);
     }
 
     private void Start()
