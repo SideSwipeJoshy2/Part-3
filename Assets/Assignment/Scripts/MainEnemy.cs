0c84a495c669c9f0d687d653f2d0c28b6e048401
DESKTOP-APTFB7I\Joshua <hargobij@sheridancollege.ca>
2024-03-30 13:39:17 -0400

    forgot to get rid of some testing code, task 1 actually complete: bullets fire, and magazine implemented

--- Assets/Assignment/Scripts/MainEnemy.cs
+++ Assets/Assignment/Scripts/MainEnemy.cs
@@ -11,8 +11,6 @@ public class MainEnemy : MonoBehaviour
 
 
 
-    protected Vector2 destination;
-    Vector2 movement;
     protected float speed = 3;
     static int ammomax = 10;
      public GameObject projectile;
@@ -20,44 +18,12 @@ public class MainEnemy : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        destination = transform.position;
-    }
-    
-
-
-    private void FixedUpdate()
-    {
-        movement = destination - (Vector2)transform.position;
-
-        //flip the x direction of the game object & children to face the direction we're walking
-        if (movement.x > 0)
-        {
-            transform.localScale = new Vector3(-1, 1, 1);
-        }
-        else if (movement.x < 0)
-        {
-            transform.localScale = new Vector3(1, 1, 1);
-        }
-
-        //stop moving if we're close enough to the target
-        if (movement.magnitude < 0.1)
-        {
-            movement = Vector2.zero;
-            speed = 3;
-        }
-        rb.MovePosition(rb.position + movement.normalized * speed * Time.deltaTime);
 
     }
-
-
 
     protected virtual void Update()
     {
-        //left click: move to the click location
-        if (Input.GetMouseButtonDown(0))
-        {
-            destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        }
+        
         if (Input.GetKey(KeyCode.Space) && ammomax>0)
         {
             Instantiate(projectile, spawn.position, spawn.rotation);
